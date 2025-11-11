e7b7be1a0ae0bba94f002e381da7a334d06689f4
Richard Aroksaar <raroksaar@me.com>
2025-11-10 21:05:33 -0800

    added python routine that does wikipedia lookups by manor name and then constructs geojson

---
+++ wikipedia_geo.py
@@ -0,0 +1,42 @@
+import wikipedia 
+import re
+from geojson import Feature, Point
+
+wikipedia.set_lang("et")
+
+# read these from file derived from the portal site,
+# after manual testing:
+portal_link= "https://www.mois.ee/jarva/albu.shtml"
+manor = "Albu mõis"
+longName ="rüütlimõis, Järva-Madise kihelkonnas Järvamaal"
+
+# wikipedia page object is created
+page_object = wikipedia.page(manor)
+
+placeName = page_object.original_title + ", " +longName
+print('Title: '+ placeName)
+
+print('first 10 links: ')
+print(page_object.coordinates)
+
+url =page_object.url
+print(url)
+
+url = '<a href="' + portal_link + '>Mõisa Poortal - Estonian Manors</a>'
+placeName = placeName + "<br>" + url
+
+coords = str(page_object.coordinates)
+
+x = re.search(".*(\d{2}\.\d{8}).*(\d{2}\.\d{8}).*", coords)
+lat = x.group(1)
+print(lat)
+
+long = x.group(2)
+print(long)
+geoj = Point((float(long), float(lat)))
+
+
+print(placeName)
+
+albu = Feature(geometry=geoj, properties={"popupContent": placeName, "Country: ": "Eesti"})
+print(str(albu))
